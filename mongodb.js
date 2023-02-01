4722b56dc3a7d167848da8882fb230df2678e59d
striker <goodluckodemilin@gmail.com>
2023-02-01 05:37:28 +0100

    fixed monogo link

--- mongodb.js
+++ mongodb.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // Set up mongoose connection
 const dev_db_url = process.env.DEV_DB;
-const mongoDb = process.env.MONGODB_URI || dev_db_url;
+const mongoDb = process.env.MONGODB_URL || dev_db_url;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
